4dba323b8e375d8f5c3b432f6ef2922597fa2332
Ngoc Truong <ngoc.truong@posteo.de>
2020-05-23 22:47:41 +0200

    Add build to backend

---
+++ build/precache-manifest.a90c05a2875b59965c41a5a0d9e0b275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da12c3cf153ac01520581921f11199eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8db44c0ff2059e19e19",
+    "url": "/static/css/main.8e368650.chunk.css"
+  },
+  {
+    "revision": "98729c98ebab0fe2a441",
+    "url": "/static/js/2.6b420a64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b420a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8db44c0ff2059e19e19",
+    "url": "/static/js/main.f7de849d.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
