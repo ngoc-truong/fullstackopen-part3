90c782e2f543bd697c0ef03aede297d9bea89640
Ngoc Truong <ngoc.truong@posteo.de>
2020-05-24 00:40:17 +0200

    uibuild

--- build/precache-manifest.49fad04684af0ff02de8df5309c0345b.js
+++ build/precache-manifest.49fad04684af0ff02de8df5309c0345b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7490a84846f6cee708973d9cfc592a12",
+    "revision": "6a83e6b9094619d98f8a55186845d13a",
     "url": "/index.html"
   },
   {
-    "revision": "e56b4b66dfd512109de8",
+    "revision": "dfd806b9afec4234a1a5",
     "url": "/static/css/main.8e368650.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6b420a64.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e56b4b66dfd512109de8",
-    "url": "/static/js/main.2d5d19ae.chunk.js"
+    "revision": "dfd806b9afec4234a1a5",
+    "url": "/static/js/main.0fb07fe0.chunk.js"
   },
   {
     "revision": "68d5127cb03992670844",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ea7ae10086ca067aa0fe9f8eff6067b.js"
+  "/precache-manifest.49fad04684af0ff02de8df5309c0345b.js"
 );
 
 self.addEventListener('message', (event) => {
