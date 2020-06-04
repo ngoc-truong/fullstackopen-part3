11975f9ba254e7ca990d1e1fe517f49d1bcc9f0d
Ngoc Truong <ngoc.truong@posteo.de>
2020-06-04 23:20:16 +0200

    Add ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true
+	},
+	"extends": "eslint:recommended",
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 11
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab",
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"eqeqeq": "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing":[
+			"error", "always"
+		],
+		"arrow-spacing":[
+			"error", { "before": true, "after": true }
+		],
+		"no-console": 0
+	}
+};
